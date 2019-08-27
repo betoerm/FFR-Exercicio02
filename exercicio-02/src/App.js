10da62d9714362a505174e895da88670e7cca2dd
betoer <betoer@gmail.com>
2019-08-26 23:12:16 -0300

    Inclusão de rotas

---
+++ exercicio-02/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { ApplicationRoutes } from "./ApplicationRoutes";
+
+export function App() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/users">Users</Link>
+        </li>
+      </ul>
+
+      <ApplicationRoutes />
+    </div>
+  );
+}
+
+export default App;
